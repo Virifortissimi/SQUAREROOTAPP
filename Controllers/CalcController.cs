5136f2e1684ab0a26e90f218a9d7ea86a5f8d63c
Virifortissimi <contacteghosa@gmail.com>
2020-05-05 18:32:11 +0100

    Handled Exceptions

--- Controllers/CalcController.cs
+++ Controllers/CalcController.cs
@@ -14,26 +14,44 @@ namespace SqrtApp.Controllers
         [HttpPost]
         public ActionResult Sqrt(string firstNumber, string secondNumber)
         {
-            int numberOne = int.Parse(firstNumber);
-            int numberTwo = int.Parse(secondNumber);
-            double firstSqrt = Math.Sqrt(numberOne);
-            double secondSqrt = Math.Sqrt(numberTwo);
-            double highestSqrt = 0;
-            if (firstSqrt > secondSqrt)
-            {
-                highestSqrt = firstSqrt;
-            }
-            else
-            {
-                highestSqrt = secondSqrt;
-            }
+            
+                try
+                {
+                    if (Convert.ToInt32(firstNumber) < 0 || Convert.ToInt32(secondNumber) < 0)
+                    {
+                        string ErrorMessage = "Please insert a positive number!!";
 
-            ViewBag.highestSqrtValue = highestSqrt;
-            ViewBag.numberOneValue = numberOne;
-            ViewBag.numberTwoValue = numberTwo;
-            ViewBag.firstSqrtValue = firstSqrt;
-            ViewBag.secondSqrtValue = secondSqrt;
-
+                        ViewBag.ErrorMessageValue = ErrorMessage;
+                    }
+                    else
+                    {
+                        int numberOne = int.Parse(firstNumber);
+                        int numberTwo = int.Parse(secondNumber);
+                        double firstSqrt = Math.Sqrt(numberOne);
+                        double secondSqrt = Math.Sqrt(numberTwo);
+                        double highestSqrt = 0;
+                        if (firstSqrt > secondSqrt)
+                        {
+                            highestSqrt = firstSqrt;
+                        }
+                        else
+                        {
+                            highestSqrt = secondSqrt;
+                        }
+                        ViewBag.Result = highestSqrt;
+                        ViewBag.numberOneValue = numberOne;
+                        ViewBag.numberTwoValue = numberTwo;
+                        ViewBag.firstSqrtValue = firstSqrt;                    
+                        ViewBag.secondSqrtValue = secondSqrt;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    // Console.WriteLine(ex.Message);
+                    var exceptionValue = ex.Message;
+                    ViewBag.FormatError = "Please Insert a number instead. " + exceptionValue;
+                }
+            
             return View();
         }
     }
